e1033339d4f603c8667c8137da6eacf7372f059d
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-17 17:52:26 +0900

    [update] user.vue作成

--- vue.js/layouts/user/User.vue
+++ vue.js/layouts/user/User.vue
@@ -3,21 +3,65 @@
         <nav-drawer />
 
         <v-main>
-            <page-title title="ユーザー名"/>
-
-            <profile />
+            <page-title title="ユーザー名" />
+            <profile :user="user" />
+            <post-review :post="posts[0]"/>
+            <post-free :post="posts[1]"/>
         </v-main>
     </v-app>
 </template>
 
 <script setup>
 import NavDrawer from '@/components/NavDrawer.vue'
-import Profile from '@/components/Profile.vue';
-import PageTitle from '@/components/PageTitle.vue';
+import PageTitle from '@/components/PageTitle.vue'
+import Profile from '@/components/Profile.vue'
+import PostReview from '@/components/PostReview.vue'
+import PostFree from '@/components/PostFree.vue'
 </script>
 
 <script>
 export default {
-    data: () =>
+    data: () => ({
+        user: {
+            id: '12345',
+            name: '雅弥',
+            link: 'link',
+            profile: 'プロフィールプロフィールプロフィールプロフィールプロフィール',
+        },
+        posts: [
+            {
+                name: "雅弥",
+                myImg: "src/assets/masaya.png",
+                title: "ZOOM/ MS-50G マルチストンプ マルチエフェクター",
+                img: "src/assets/ms50g.png",
+                music1: "src/assets/maou_bgm_piano40.mp3",
+                music2: "src/assets/maou_bgm_fantasy15.mp3",
+                recording: "PRESONUS Studio 24cからPCに取り込みました。DTMのソフトはStudio one5のArtistを使用しました。",
+                items: [
+                    { text: 'YAMAHA REVSTAR420' },
+                    { text: 'CANARE シールド' },
+                    { text: 'BOSS MS-50g' },
+                    { text: 'CANARE シールド' },
+                    { text: 'PRESONUS Studio 24c' },
+                ],
+            },
+            {
+                name: "雅弥",
+                myImg: "src/assets/masaya.png",
+                title: "エレキギターをアコギの音に！！！",
+                img: "src/assets/AC-3.jpg",
+                music: "src/assets/maou_bgm_acoustic54.mp3",
+                recording: "PRESONUS Studio 24cからPCに取り込みました。DTMのソフトはStudio one5のArtistを使用しました。",
+                items: [
+                    { text: 'YAMAHA REVSTAR420' },
+                    { text: 'CANARE シールド' },
+                    { text: 'BOSS AC-3' },
+                    { text: 'BOSS Equalizer GE-7' },
+                    { text: 'CANARE シールド' },
+                    { text: 'PRESONUS Studio 24c' },
+                ],
+            },
+        ]
+    }),
 }
 </script>
