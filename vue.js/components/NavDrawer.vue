aaa3c4ece8137ccd77f7b4d7362953f4fd0bb313
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-18 11:51:25 +0900

    [update] settingsのパスを部分一致に変更

--- vue.js/components/NavDrawer.vue
+++ vue.js/components/NavDrawer.vue
@@ -13,8 +13,8 @@
                     "/inquiry" ? "$email" : "$emailOutline" }}</v-icon></v-list-item>
                 <v-list-item to="/user" :ripple="false" :active="false" class="text-center pa-2 ml-auto" width="50px" height="50px"><v-icon class="text-h4">{{ path ===
                     "/user" ? "$account" : "$accountOutline" }}</v-icon></v-list-item>
-                <v-list-item to="/settings/notice_setting" :ripple="false" :active="false" class="text-center pa-2 ml-auto" width="50px" height="50px"><v-icon class="text-h4">{{ path
-                    === "/settings" ? "$cog" : "$cogOutline" }}</v-icon></v-list-item>
+                <v-list-item to="/settings/notice_setting" :ripple="false" :active="false" class="text-center pa-2 ml-auto" width="50px" height="50px"><v-icon class="text-h4">{{ path.indexOf('/settings')
+                    === 0 ? "$cog" : "$cogOutline" }}</v-icon></v-list-item>
                 <v-list-item @click="onPost" :ripple="false" :active="false" class="text-center pa-2 ml-auto" width="50px" height="50px" value="post"><v-icon class="text-h4">$guitarPickOutline</v-icon></v-list-item>
                 <v-list-item @click="onLogout" :ripple="false" :active="false" class="text-center pa-2 ml-auto" width="50px" height="50px"><v-icon
                         class="text-h4">$logout</v-icon></v-list-item>
