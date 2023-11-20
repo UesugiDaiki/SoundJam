3984a6d29747096d795f4f0bb28ff79f8ae5be1d
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-20 15:13:05 +0900

    [add] お問い合わせダイアログ作成

--- vue.js/components/InquiryButton.vue
+++ vue.js/components/InquiryButton.vue
@@ -1,5 +1,21 @@
 <template>
-    <v-btn to="/inquiry/form" class="rounded-circle" height="64px" width="64px" style="z-index: 2; bottom: 50px; right: 50px; position:fixed;">
+    <v-btn @click="onInquiry" class="rounded-circle" height="64px" width="64px" style="z-index: 2; bottom: 50px; right: 50px; position:fixed;">
         <v-icon class="text-h4">$pencil</v-icon>
     </v-btn>
+
+    <inquiry-dialog ref="inquiry"/>
 </template>
+
+<script setup>
+import InquiryDialog from './InquiryDialog.vue'
+</script>
+
+<script>
+export default {
+    methods: {
+        onInquiry() {
+            this.$refs.inquiry.openInquiry()
+        },
+    }
+}
+</script>
