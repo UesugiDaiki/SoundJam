4cc5be00806d3c646dfd2a0475d041087d55dba0
MasayaSonoda <machiya5454@gmail.com>
2023-11-22 09:35:40 +0900

    [update] リンク修正

--- vue.js/components/Product.vue
+++ vue.js/components/Product.vue
@@ -1,7 +1,7 @@
 <template>
         <v-row class="pl-3" justify="center">
             <v-card justify="center" class="mx-auto px-4 py-2" width="570px" max-width="570" min-width="200" rounded="0" elevation="0"
-                style="border: 1px solid #ddd;" link to="/productDetails">
+                style="border: 1px solid #ddd;" link to="/detail">
                 <v-card-title class="d-flex pa-0">
                     <v-avatar :image="product.img" size="47" ></v-avatar>
                     <v-card-text style="font-size: 20px;" >
