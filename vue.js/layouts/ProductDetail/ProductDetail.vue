85812c0acd671936b01d0f227e9f13089a39b6d4
MasayaSonoda <machiya5454@gmail.com>
2023-11-22 09:33:35 +0900

    [add] ProductDetail.vue(製品詳細画面)を追加しました

---
+++ vue.js/layouts/ProductDetail/ProductDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+      <nav-drawer />
+      <page-title />
+      <v-main>
+        <product-detail />
+      </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import PageTitle from '@/components/PageTitle.vue'
+import NavDrawer from '@/components/NavDrawer.vue'
+import ProductDetail from '@/components/ProductDetail.vue'
+</script>
+
