29ffbc8aaddda0a1daf37f380d7f3d776e50d9f7
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-24 01:06:41 +0900

    [update] 投稿のループ表示

--- vue.js/layouts/user/Edit.vue
+++ vue.js/layouts/user/Edit.vue
@@ -24,40 +24,6 @@ export default {
             link: 'link',
             profile: 'プロフィールプロフィールプロフィールプロフィールプロフィール',
         },
-        posts: [
-            {
-                name: "雅弥",
-                myImg: "src/assets/masaya.png",
-                title: "ZOOM/ MS-50G マルチストンプ マルチエフェクター",
-                img: "src/assets/ms50g.png",
-                music1: "src/assets/maou_bgm_piano40.mp3",
-                music2: "src/assets/maou_bgm_fantasy15.mp3",
-                recording: "PRESONUS Studio 24cからPCに取り込みました。DTMのソフトはStudio one5のArtistを使用しました。",
-                items: [
-                    { text: 'YAMAHA REVSTAR420' },
-                    { text: 'CANARE シールド' },
-                    { text: 'BOSS MS-50g' },
-                    { text: 'CANARE シールド' },
-                    { text: 'PRESONUS Studio 24c' },
-                ],
-            },
-            {
-                name: "雅弥",
-                myImg: "src/assets/masaya.png",
-                title: "エレキギターをアコギの音に！！！",
-                img: "src/assets/AC-3.jpg",
-                music: "src/assets/maou_bgm_acoustic54.mp3",
-                recording: "PRESONUS Studio 24cからPCに取り込みました。DTMのソフトはStudio one5のArtistを使用しました。",
-                items: [
-                    { text: 'YAMAHA REVSTAR420' },
-                    { text: 'CANARE シールド' },
-                    { text: 'BOSS AC-3' },
-                    { text: 'BOSS Equalizer GE-7' },
-                    { text: 'CANARE シールド' },
-                    { text: 'PRESONUS Studio 24c' },
-                ],
-            },
-        ]
     }),
 }
 </script>
