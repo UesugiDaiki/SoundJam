c818f2cf1f284cdebc9b03326fc85825a72d425f
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-24 00:29:53 +0900

    [update] いろいろ

--- vue.js/components/PageTitle.vue
+++ vue.js/components/PageTitle.vue
@@ -1,11 +1,6 @@
 <template>
     <v-app-bar color="white" elevation="0">
-        <v-btn
-        class="ma-2"
-        variant="text"
-        icon="$arrowLeft"
-        onclick="window.history.back();"
-      ></v-btn>
+        <v-btn class="ma-2" variant="text" icon="$arrowLeft" onclick="window.history.back();"></v-btn>
         <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-app-bar>
 </template>
