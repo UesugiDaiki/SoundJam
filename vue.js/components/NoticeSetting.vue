a5a5a06a3b144169291d623850358c1c24b851c1
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-24 01:31:44 +0900

    [update] 設定画面の細かい修正

--- vue.js/components/NoticeSetting.vue
+++ vue.js/components/NoticeSetting.vue
@@ -1,34 +1,34 @@
 <template>
-    <v-list-item-title> <span class="mx-4 mt-5 text-h5">通知設定</span> </v-list-item-title>
-    <v-divider></v-divider>
-    <v-card rounded="0" elevation="0" class="mt-3 pb-6">
-        <v-card-title class="d-flex pb-0">
-            <v-sheet height="31" width="100" class="my-auto">
-                フォロー通知
-            </v-sheet>
-            <v-spacer></v-spacer>
-            <v-switch color="primary" :model-value="true" label="ON" hide-details></v-switch>
-        </v-card-title>
-        <v-card-subtitle>
-            他のユーザーからフォローされた際に通知します
-        </v-card-subtitle>
-    </v-card>
-    <v-divider></v-divider>
-    <v-card rounded="0" elevation="0" class="mt-3 pb-6">
-        <v-card-title class="d-flex pb-0">
-            <v-sheet height="31" width="100" class="my-auto">
-                いいね通知
-            </v-sheet>
-            <v-spacer></v-spacer>
-            <v-switch color="primary" :model-value="true" label="ON" hide-details class=""></v-switch>
-        </v-card-title>
-        <v-card-subtitle>
-            (仮テキスト)他のユーザーからいいねされた際に通知します
-
-        </v-card-subtitle>
-    </v-card>
+    <v-sheet max-width="540">
+        <v-list-item-title class="ma-4 text-h6">通知設定</v-list-item-title>
+        <v-divider></v-divider>
+        <v-card rounded="0" elevation="0" class="mt-3 pb-6">
+            <v-card-title class="d-flex pb-0">
+                <v-sheet height="31" width="100" class="my-auto text-subtitle-1">
+                    フォロー通知
+                </v-sheet>
+                <v-spacer></v-spacer>
+                <v-switch color="primary" :model-value="true" label="ON" hide-details></v-switch>
+            </v-card-title>
+            <v-card-subtitle>
+                他のユーザーからフォローされた際に通知します
+            </v-card-subtitle>
+        </v-card>
+        <v-divider></v-divider>
+        <v-card rounded="0" elevation="0" class="mt-3 pb-6">
+            <v-card-title class="d-flex pb-0">
+                <v-sheet height="31" width="100" class="my-auto text-subtitle-1">
+                    いいね通知
+                </v-sheet>
+                <v-spacer></v-spacer>
+                <v-switch color="primary" :model-value="true" label="ON" hide-details class=""></v-switch>
+            </v-card-title>
+            <v-card-subtitle>
+                (仮テキスト)他のユーザーからいいねされた際に通知します
+            </v-card-subtitle>
+        </v-card>
+    </v-sheet>
 </template>
-
 
 <script>
 export default {
--- vue.js/layouts/settings/Settings.vue
+++ vue.js/layouts/settings/Settings.vue
@@ -1,12 +1,11 @@
 <template>
     <v-app>
         <nav-drawer />
-
+        
         <v-main>
+            <page-title title="設定"/>
             <v-navigation-drawer permanent>
                 <v-list>
-                    <v-list-item-title> <span class="mx-4 mt-5 text-h5">設定</span> </v-list-item-title>
-                    <v-divider></v-divider>
                     <v-list-item to="/settings/notice_setting" title="通知設定" value="1"></v-list-item>
                     <v-list-item to="/settings/reset_password" title="パスワード再設定" value="2"></v-list-item>
                 </v-list>
@@ -18,4 +17,5 @@
 
 <script setup>
 import NavDrawer from '@/components/NavDrawer.vue'
+import PageTitle from '@/components/PageTitle.vue';
 </script>
