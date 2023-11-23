a87f1f1d7f5984c774b2667f7b3d0cf8ec255af2
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-24 00:25:50 +0900

    [update] 投稿のループ表示

---
+++ vue.js/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+    <post-review v-if="post.postType" :post="post"/>
+    <post-free v-else :post="post"/>
+</template>
+
+<script setup>
+import PostReview from './PostReview.vue';
+import PostFree from './PostFree.vue';
+</script>
+
+<script>
+export default {
+    props: {
+        post: Object,
+    }
+}
+</script>
