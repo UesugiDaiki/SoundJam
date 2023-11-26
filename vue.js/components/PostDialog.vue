29c7c43c88db60c9c0c5b2de886655b7aa3243ce
UesugiDaiki <koreha.apexyo@gmail.com>
2023-11-26 18:28:17 +0900

    [update] 投稿画面のレイアウト変更、機材の追加削除機能追加

--- vue.js/components/PostDialog.vue
+++ vue.js/components/PostDialog.vue
@@ -18,35 +18,37 @@
                         <v-window-item value="free">
                             <form @submit.prevent="submit">
                                 <v-row>
-                                    <!-- 左上 -->
                                     <v-col cols="6" class="pb-0">
-                                        <v-text-field label="Legal first name*" required></v-text-field>
-                                        <v-text-field label="Legal first name*" required></v-text-field>
-                                        <v-textarea rows="2" label="Label"></v-textarea>
-                                        <v-textarea rows="2" label="Label"></v-textarea>
+                                        <v-text-field v-model="free.product" label="タイトル" required></v-text-field>
                                     </v-col>
-                                    <!-- 右上 -->
                                     <v-col cols="6" class="pb-0">
-                                        <v-col class="pa-0" cols="12" style="height: 156px;">
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth" required ></v-file-input>
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth" required ></v-file-input>
-                                        </v-col>
-                                        <v-col class="pa-0" cols="12">
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$camera" required></v-file-input>
-                                        </v-col>
+                                        <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth"
+                                        hint="音声" required></v-file-input>
                                     </v-col>
-                                    <!-- 左下 -->
+                                    <v-col cols="6" class="py-0">
+                                        <v-file-input prepend-icon="" prepend-inner-icon="$camera" hint="つまみの状態がわかる画像"
+                                            required></v-file-input>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col cols="6" class="py-0">
+                                        <v-textarea v-model="free.overview" rows="2" label="概要"></v-textarea>
+                                    </v-col>
+                                    <v-col cols="6" class="py-0">
+                                        <v-textarea v-model="free.recordingMethod" rows="2" label="録音方法"></v-textarea>
+                                    </v-col>
                                     <v-col cols="6" class="pt-0">
-                                        <v-text-field v-for="product in freeProducts" :label="String(product.id)"></v-text-field>
-                                        <v-btn variant="flat" icon="$plus"></v-btn>
+                                        <v-text-field v-for="product in free.products"
+                                            :hint="'楽器から' + String(product.index + 1) + 'つ目につなげた機材名'" :label="'機材' + String(product.index + 1)"></v-text-field>
+                                        <v-btn variant="flat" icon="$plus" @click="addProduct(tab)"></v-btn>
                                     </v-col>
-                                    <!-- 右下 -->
                                     <v-col cols="6" class="pt-0 d-flex align-end" style="padding-bottom: 86px;">
-                                        <v-btn v-if="freeProducts.length > 1" variant="flat" icon="$minus"></v-btn>
+                                        <v-btn v-if="free.products.length > 1" variant="flat" icon="$minus" @click="removeProduct(tab)"></v-btn>
                                     </v-col>
                                 </v-row>
 
-                                <v-card-actions>
+
+                                <v-card-actions class="mt-4">
                                     <v-btn variant="flat" class="me-4" type="submit" color="primary">
                                         投稿
                                     </v-btn>
@@ -60,35 +62,41 @@
                         <v-window-item value="review">
                             <form @submit.prevent="submit">
                                 <v-row>
-                                    <!-- 左上 -->
                                     <v-col cols="6" class="pb-0">
-                                        <v-text-field label="Legal first name*" required></v-text-field>
-                                        <v-text-field label="Legal first name*" required></v-text-field>
-                                        <v-textarea rows="2" label="Label"></v-textarea>
-                                        <v-textarea rows="2" label="Label"></v-textarea>
+                                        <v-text-field v-model="review.product" label="製品名" required></v-text-field>
                                     </v-col>
-                                    <!-- 右上 -->
                                     <v-col cols="6" class="pb-0">
-                                        <v-col class="pa-0" cols="12" style="height: 156px;">
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth" required ></v-file-input>
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth" required ></v-file-input>
-                                        </v-col>
-                                        <v-col class="pa-0" cols="12">
-                                            <v-file-input prepend-icon="" prepend-inner-icon="$camera" required></v-file-input>
-                                        </v-col>
+                                        <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth"
+                                        hint="音声（エフェクターOFF）" required></v-file-input>
                                     </v-col>
-                                    <!-- 左下 -->
+                                    <v-col cols="6" class="py-0">
+                                        <v-file-input prepend-icon="" prepend-inner-icon="$camera" hint="つまみの状態がわかる画像"
+                                            required></v-file-input>
+                                    </v-col>
+                                    <v-col cols="6" class="py-0">
+                                        <v-file-input prepend-icon="" prepend-inner-icon="$musicNoteEighth"
+                                        hint="音声（エフェクターON）" required></v-file-input>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col cols="6" class="py-0">
+                                        <v-textarea v-model="review.overview" rows="2" label="概要"></v-textarea>
+                                    </v-col>
+                                    <v-col cols="6" class="py-0">
+                                        <v-textarea v-model="review.recordingMethod" rows="2" label="録音方法"></v-textarea>
+                                    </v-col>
                                     <v-col cols="6" class="pt-0">
-                                        <v-text-field v-for="product in reviewProducts" :label="String(product.id)"></v-text-field>
-                                        <v-btn variant="flat" icon="$plus"></v-btn>
+                                        <v-text-field v-for="product in review.products"
+                                            :hint="'楽器から' + String(product.index + 1) + 'つ目につなげた機材名'" :label="'機材' + String(product.index + 1)"></v-text-field>
+                                        <v-btn variant="flat" icon="$plus" @click="addProduct(tab)"></v-btn>
                                     </v-col>
-                                    <!-- 右下 -->
                                     <v-col cols="6" class="pt-0 d-flex align-end" style="padding-bottom: 86px;">
-                                        <v-btn v-if="reviewProducts.length > 1" variant="flat" icon="$minus"></v-btn>
+                                        <v-btn v-if="review.products.length > 1" variant="flat" icon="$minus" @click="removeProduct(tab)"></v-btn>
                                     </v-col>
                                 </v-row>
 
-                                <v-card-actions>
+
+                                <v-card-actions class="mt-4">
                                     <v-btn variant="flat" class="me-4" type="submit" color="primary">
                                         投稿
                                     </v-btn>
@@ -110,19 +118,48 @@ export default {
         return {
             dialog: false,
             tab: null,
-            freeProducts: [
-                {id: 0, item: '製品名１'},
-                {id: 1, item: '製品名２'}
-            ],
-            reviewProducts: [
-                {id: 0, item: '製品名１'},
-            ],
+            free: {
+                title: "",
+                overview: "",
+                recordingMethod: "",
+                products: [
+                    {index: 0, product: ""},
+                ],
+            },
+            review: {
+                product: "",
+                overview: "",
+                recordingMethod: "",
+                products: [
+                    {index: 0, product: ""},
+                ],
+            },
         }
     },
     methods: {
         openPost() {
             this.dialog = true
-        }
+        },
+        // 使用機材追加
+        addProduct(type) {
+            if (type === 'free') {
+                let newIndex = this.free.products.length
+                let newProduct = {index: newIndex, product: ''}
+                this.free.products.push(newProduct)
+            } else if (type === 'review') {
+                let newIndex = this.review.products.length
+                let newProduct = {index: newIndex, product: ''}
+                this.review.products.push(newProduct)
+            }
+        },
+        // 使用機材削除
+        removeProduct(type) {
+            if (type === 'free') {
+                this.free.products.pop()
+            } else if (type === 'review') {
+                this.review.products.pop()
+            }
+        },
     }
 }
 </script>
