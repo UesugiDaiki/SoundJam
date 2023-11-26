ddc87c82d43133063929d935a326e32aaf646434
amor0000 <amor004218@gmail.com>
2023-11-26 18:18:15 +0900

    [update] Main.vueのRouterviewを変更

--- laravel/inertia/resources/js/Pages/Main.vue
+++ laravel/inertia/resources/js/Pages/Main.vue
@@ -1,5 +1,12 @@
 <template>
-    <router-view />
+    <!-- <router-view /> -->
+    <RouterView v-slot="{ Component }">
+    <Suspense>
+        <div>
+        <component :is="Component" />
+        </div>
+    </Suspense>
+    </RouterView>
 </template>
 
 <script setup>
