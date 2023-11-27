b9147f105502606e2d93f4020f59ed290881a6d8
8311yua <127169709+8311yua@users.noreply.github.com>
2023-11-27 12:03:04 +0900

    age

--- laravel/inertia/app/Http/Controllers/UserController.php
+++ laravel/inertia/app/Http/Controllers/UserController.php
@@ -9,10 +9,22 @@ use App\Models\User;
 class UserController extends Controller
 {
     //
-    public function index()
-    {
-        // $users = User::all();
-        $users = DB::select('select * from users');
+    public function index(){
+        // JOIN操作を使用してuser_table、post_table、product_tableのデータを結合
+        $users = DB::select('
+            SELECT
+                user_table.*,
+                post_table.*,
+                product_table.*
+            FROM
+                post_table
+            JOIN
+                user_table ON post_table.USER_ID = user_table.id
+            LEFT JOIN
+                product_table ON post_table.PRODUCT_ID = product_table.id
+        ');
+
+
         return $users;
     }
 }
