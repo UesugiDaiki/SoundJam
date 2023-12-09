4e30de5ab3e43a913298b5b68e3c6015016ea64d
UesugiDaiki <koreha.apexyo@gmail.com>
2023-12-09 16:38:15 +0900

    [fix] 画面縮小時、投稿と区切りのバーが横並びになる不具合修正

--- laravel/inertia/resources/js/components/PostFree.vue
+++ laravel/inertia/resources/js/components/PostFree.vue
@@ -1,47 +1,49 @@
 <template>
-<v-row class="pl-3" justify="center">
-    <v-col cols="auto" class="soto">
-    <v-card class="mx-auto" width="570px" max-width="570" min-width="200" rounded="0" elevation="0"
-        link :ripple="false" :to="{name: 'post', params: {postId: this.post.id}}" @click="setPostDetail">
-        <Title :name="post.USER_NAME" :myImg="'../../'+post.ICON" :userId="post.USER_ID"/>
-        <v-card-title>
-        {{ post.TITLE }}
-        </v-card-title>
-        <div class="aaa">
-        <v-img max-width="250" min-width="250" max-height="190" min-height="190" :src="'../../'+post.IMAGES"></v-img>
-        <div class="audio">
-            <audio controlslist="nodownload" class="audio-position-free" controls :src="'../../' + post.AUDIO1"></audio>
-        </div>
-        </div>
-        <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn :icon="show ? '$chevronUp' : '$chevronDown'" @click="accodion($event)"></v-btn>
-        </v-card-actions>
-        <v-expand-transition>
-        <div v-show="show">
-            <v-row no-gutters>
-                <div class="add1">
-                <h2>
-                    使用機材
-                </h2>
-                <v-list v-for="item in post.ITEMS" :key="item" :value="item" color="primary">
-                    <v-list-item-title :padding="0">■{{ item }}</v-list-item-title>
-                </v-list>
+    <v-row class="pl-3" justify="center">
+        <v-col cols="auto" class="soto">
+            <v-card class="mx-auto" width="570px" max-width="570" min-width="200" rounded="0" elevation="0" link
+                :ripple="false" :to="{ name: 'post', params: { postId: this.post.id } }" @click="setPostDetail">
+                <Title :name="post.USER_NAME" :myImg="'../../' + post.ICON" :userId="post.USER_ID" />
+                <v-card-title>
+                    {{ post.TITLE }}
+                </v-card-title>
+                <div class="aaa">
+                    <v-img max-width="250" min-width="250" max-height="190" min-height="190"
+                        :src="'../../' + post.IMAGES"></v-img>
+                    <div class="audio">
+                        <audio controlslist="nodownload" class="audio-position-free" controls
+                            :src="'../../' + post.AUDIO1"></audio>
+                    </div>
                 </div>
-                <v-divider vertical class="mx-4 border-opacity-25" inset></v-divider>
-                <div class="add2">
-                <h2>
-                    録音方法
-                </h2>
-                    {{ post.RECORDING_METHOD }}
-                </div>
-            </v-row>
-        </div>
-        </v-expand-transition>
-    </v-card>
-    </v-col>
-    <v-divider length="570"></v-divider>
-</v-row>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn :icon="show ? '$chevronUp' : '$chevronDown'" @click="accodion($event)"></v-btn>
+                </v-card-actions>
+                <v-expand-transition>
+                    <div v-show="show">
+                        <v-row no-gutters>
+                            <div class="add1">
+                                <h2>
+                                    使用機材
+                                </h2>
+                                <v-list v-for="item in post.ITEMS" :key="item" :value="item" color="primary">
+                                    <v-list-item-title :padding="0">■{{ item }}</v-list-item-title>
+                                </v-list>
+                            </div>
+                            <v-divider vertical class="mx-4 border-opacity-25" inset></v-divider>
+                            <div class="add2">
+                                <h2>
+                                    録音方法
+                                </h2>
+                                {{ post.RECORDING_METHOD }}
+                            </div>
+                        </v-row>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-divider length="570"></v-divider>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
@@ -64,12 +66,12 @@ export default {
     }),
     methods: {
         // 使用機材、録音方法を開くとき遷移しない
-        accodion: function(event) {
+        accodion: function (event) {
             event.preventDefault()
             this.show = !this.show
         },
         // /postにリダイレクトする
-        toPost: function() {
+        toPost: function () {
             // this.$router.push("/post");
             console.log('関数遷移成功')
             // postに遷移した時、この投稿のidを渡す
@@ -89,49 +91,49 @@ export default {
 
 <style>
 .aaa {
-display: flex;
-gap: 6px;
-padding-left: 6px;
+    display: flex;
+    gap: 6px;
+    padding-left: 6px;
 }
 
 .center {
-float: left;
+    float: left;
 }
 
 .v-card--reveal {
-bottom: 0;
-opacity: 1 !important;
-position: absolute;
-width: 100%;
+    bottom: 0;
+    opacity: 1 !important;
+    position: absolute;
+    width: 100%;
 }
 
 .audio {
-width: 300px;
+    width: 300px;
 }
 
 .audio-position-free {
-margin-top: 80px;
+    margin-top: 80px;
 }
 
 .add1 {
-width: 250px;
-margin-left: 20px;
+    width: 250px;
+    margin-left: 20px;
 }
 
 .add2 {
-width: 250px;
+    width: 250px;
 }
 
 .text-length {
-height: 120px;
-overflow: hidden;
-display: -webkit-box;
--webkit-box-orient: vertical;
--webkit-line-clamp: 6;
-/* 任意の行数を指定 */
+    height: 120px;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 6;
+    /* 任意の行数を指定 */
 }
 
 .soto {
-padding: 0;
+    padding: 0;
 }
 </style>
--- laravel/inertia/resources/js/components/PostReview.vue
+++ laravel/inertia/resources/js/components/PostReview.vue
@@ -1,49 +1,52 @@
 <template>
-<v-row class="pl-3" justify="center">
-    <v-col cols="auto" class="soto">
-    <v-card class="mx-auto" width="570px" max-width="570" min-width="200" rounded="0" elevation="0"
-        link :ripple="false" @click="setPostDetail" :to="{name: 'post', params: {postId: this.post.id}}" >
-        <Title :name="post.USER_NAME" :myImg="'../../'+post.ICON" :userId="post.USER_ID"/>
-        <v-card-title>
-        {{ post.TITLE }}
-        <div class="Item-Content-Flg4"></div>
-        </v-card-title>
-        <div class="aaa">
-        <v-img max-width="250" min-width="250" max-height="190" min-height="190" :src="'../../'+post.IMAGES"></v-img>
-        <div class="audio">
-            <audio controlslist="nodownload" class="audio-position" controls :src="'../../' + post.AUDIO1"></audio>
-            <audio controlslist="nodownload" class="audio-position" controls :src="'../../' + post.AUDIO2"></audio>
-        </div>
-        </div>
-        <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn :icon="show ? '$chevronUp' : '$chevronDown'" @click="accodion($event)"></v-btn>
-        </v-card-actions>
-        <v-expand-transition>
-        <div v-show="show">
-            <v-row no-gutters>
-                <div class="add1">
-                <h2>
-                    使用機材
-                </h2>
-                <v-list v-for="item in post.ITEMS" :key="item" :value="item" color="primary">
-                    <v-list-item-title :padding="0">■{{ item }}</v-list-item-title>
-                </v-list>
+    <v-row class="pl-3" justify="center">
+        <v-col cols="auto" class="soto">
+            <v-card class="mx-auto" width="570px" max-width="570" min-width="200" rounded="0" elevation="0" link
+                :ripple="false" @click="setPostDetail" :to="{ name: 'post', params: { postId: this.post.id } }">
+                <Title :name="post.USER_NAME" :myImg="'../../' + post.ICON" :userId="post.USER_ID" />
+                <v-card-title>
+                    {{ post.TITLE }}
+                    <div class="Item-Content-Flg4"></div>
+                </v-card-title>
+                <div class="aaa">
+                    <v-img max-width="250" min-width="250" max-height="190" min-height="190"
+                        :src="'../../' + post.IMAGES"></v-img>
+                    <div class="audio">
+                        <audio controlslist="nodownload" class="audio-position" controls
+                            :src="'../../' + post.AUDIO1"></audio>
+                        <audio controlslist="nodownload" class="audio-position" controls
+                            :src="'../../' + post.AUDIO2"></audio>
+                    </div>
                 </div>
-                <v-divider vertical class="mx-4 border-opacity-25" inset></v-divider>
-                <div class="add2">
-                <h2>
-                    録音方法
-                </h2>
-                    {{ post.RECORDING_METHOD }}
-                </div>
-            </v-row>
-        </div>
-        </v-expand-transition>
-    </v-card>
-    </v-col>
-    <v-divider length="570"></v-divider>
-</v-row>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn :icon="show ? '$chevronUp' : '$chevronDown'" @click="accodion($event)"></v-btn>
+                </v-card-actions>
+                <v-expand-transition>
+                    <div v-show="show">
+                        <v-row no-gutters>
+                            <div class="add1">
+                                <h2>
+                                    使用機材
+                                </h2>
+                                <v-list v-for="item in post.ITEMS" :key="item" :value="item" color="primary">
+                                    <v-list-item-title :padding="0">■{{ item }}</v-list-item-title>
+                                </v-list>
+                            </div>
+                            <v-divider vertical class="mx-4 border-opacity-25" inset></v-divider>
+                            <div class="add2">
+                                <h2>
+                                    録音方法
+                                </h2>
+                                {{ post.RECORDING_METHOD }}
+                            </div>
+                        </v-row>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-divider length="570"></v-divider>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
@@ -59,11 +62,11 @@ export default {
         show: false,
         reveal: false,
     }),
-    methods:{
+    methods: {
         // toPost: function() {
         // this.$router.push("/post");
         // },
-        accodion: function(event) {
+        accodion: function (event) {
             event.preventDefault()
             this.show = !this.show
         },
@@ -81,59 +84,58 @@ export default {
 
 <style>
 .aaa {
-display: flex;
-gap: 6px;
-padding-left: 6px;
+    display: flex;
+    gap: 6px;
+    padding-left: 6px;
 }
 
 .center {
-float: left;
+    float: left;
 }
 
 .v-card--reveal {
-bottom: 0;
-opacity: 1 !important;
-position: absolute;
-width: 100%;
+    bottom: 0;
+    opacity: 1 !important;
+    position: absolute;
+    width: 100%;
 }
 
 .audio {
-width: 300px;
+    width: 300px;
 }
 
 .audio-position {
-margin-top: 20px;
+    margin-top: 20px;
 }
 
 .add1 {
-width: 250px;
-margin-left: 20px;
+    width: 250px;
+    margin-left: 20px;
 }
 
 .add2 {
-width: 250px;
+    width: 250px;
 }
 
 .text-length {
-height: 120px;
-overflow: hidden;
-display: -webkit-box;
--webkit-box-orient: vertical;
--webkit-line-clamp: 6;
-/* 任意の行数を指定 */
+    height: 120px;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 6;
+    /* 任意の行数を指定 */
 }
 
 .soto {
-padding: 0;
+    padding: 0;
 }
 
 .Item-Content-Flg4 {
-/*三角形右上*/
-position: absolute;
-top: 0;
-right: 0;
-border-top: 40px solid #5bc8ac;
-border-left: 40px solid transparent;
+    /*三角形右上*/
+    position: absolute;
+    top: 0;
+    right: 0;
+    border-top: 40px solid #5bc8ac;
+    border-left: 40px solid transparent;
 }
-
 </style>
