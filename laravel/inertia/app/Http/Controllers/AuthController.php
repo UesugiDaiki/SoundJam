c29b91500e217951bbd1ffccf5e31718da8297e2
UesugiDaiki <koreha.apexyo@gmail.com>
2023-12-11 10:53:45 +0900

    [update] ユーザー新規作成追加

--- laravel/inertia/app/Http/Controllers/AuthController.php
+++ laravel/inertia/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 
 /* ログイン、ログアウト、新規登録など　認証機能関連 */
@@ -28,16 +29,16 @@ class AuthController extends Controller
             $user = DB::select("SELECT * FROM user_table WHERE EMAIL_ADDRESS='" . $request["loginID"] . "'");
             //デバッグ
             Log::debug($user);
-            $user[0] = (array)$user[0];
-            if ($user[0]["PASSWORDS"] == $request["loginPass"]) {
-                Session::put('soundjam_user', $user[0]["id"]);
+            $user = (array)$user[0];
+            if ($user["PASSWORDS"] == $request["loginPass"]) {
+                Session::put('soundjam_user', $user["id"]);
             }
         } else {
             // ログインIDでログイン
             $user = DB::select('SELECT * FROM user_table WHERE ID=' . $request["loginID"]);
-            $user[0] = (array)$user[0];
-            if ($user[0]["PASSWORDS"] == $request["loginPass"]) {
-                Session::put('soundjam_user', $user[0]["id"]);
+            $user = (array)$user[0];
+            if ($user["PASSWORDS"] == $request["loginPass"]) {
+                Session::put('soundjam_user', $user["id"]);
             }
         }
         Log::debug((Session::all()));
@@ -48,5 +49,26 @@ class AuthController extends Controller
     public function logout()
     {
         Session::forget('soundjam_user');
+    }
+
+    // 新規登録
+    public function regist(Request $request)
+    {
+        // DB追加
+        $regist_data = [
+            'USER_NAME' => $request->input("registName"),
+            'PROFILES' => null,
+            'WEBSITE' => null,
+            'ICON' => 'storage/user/default_icon.jpg',
+            'EMAIL_ADDRESS' => $request->input("registMail"),
+            'PASSWORDS' => $request->input("registPass"),
+            'FOLLOW_NOTICE' => 1,
+            'LIKE_NOTICE' => 1,
+            'FROZEN' => 0
+        ];
+        $user_id = DB::table('user_table')->insertGetId($regist_data);
+
+        // ユーザーディレクトリ作成
+        Storage::makeDirectory('user/' . $user_id);
     }
 }
