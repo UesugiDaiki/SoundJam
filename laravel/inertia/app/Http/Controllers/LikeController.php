2ab700aae92e19b8440a42e1c41f9e6519a59f6b
amor0000 <fides5952.free@gmail.com>
2023-12-13 09:41:47 +0900

    [add]いいね用コントローラ追加

---
+++ laravel/inertia/app/Http/Controllers/LikeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Log;
+
+class LikeController extends Controller
+{
+    //
+}
