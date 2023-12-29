fd9c81a8bc9aab1a461254dd25ff76cd5580602e
MasayaSonoda <machiya5454@gmail.com>
2023-12-29 15:37:37 +0900

    [add] 管理者ページ

---
+++ admin/kari/server_connection.php
@@ -0,0 +1,13 @@
+<?php error_reporting(0); ?>
+<?php
+$pdo=new PDO(
+  'mysql:host=localhost;dbname=test_db;charset=utf8',
+  'ADMINS',
+  'PASSWORDS'
+);
+// $pdo=new PDO(
+//   'mysql:host=mysql215.phy.lolipop.lan;dbname=LAA1530773-cafe22teamd;charset=utf8',
+//   'LAA1530773',
+//   'Fic20222023It'
+// );
+?>
