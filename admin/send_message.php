9130bed45b5987fc0bc960d6139bc25667c5da7e
MasayaSonoda <machiya5454@gmail.com>
2024-01-17 09:48:46 +0900

    [update] 入力項目を必須に

--- admin/send_message.php
+++ admin/send_message.php
@@ -8,15 +8,15 @@ require 'header.php';
     <form class="m-4" action="send_message.php" method="post">
         <div class="mb-3">
             <label for="address" class="form-label">宛先</label>
-            <input type="text" class="form-control" id="address" name="address">
+            <input type="number" class="form-control" id="address" name="address" required>
         </div>
         <div class="mb-3">
             <label for="subject" class="form-label">件名</label>
-            <input type="text" class="form-control" id="subject" name="subject">
+            <input type="text" class="form-control" id="subject" name="subject" required>
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">本文</label>
-            <textarea class="form-control" id="body" rows="7" name="body"></textarea>
+            <textarea class="form-control" id="body" rows="7" name="body" required></textarea>
         </div>
         <input type="hidden" name="command" value="send-message">
         <button type="submit" class="btn btn-primary">送信</button>
