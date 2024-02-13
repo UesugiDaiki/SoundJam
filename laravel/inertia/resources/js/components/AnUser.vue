e66151d804a1deeb0d154fd0b2a245dfbc52a651
hasiguti-t <fuy46606@gmail.com>
2024-02-13 12:33:16 +0900

    [update]検索画面のアカウント部分のレスポンシブ化

--- laravel/inertia/resources/js/components/AnUser.vue
+++ laravel/inertia/resources/js/components/AnUser.vue
@@ -1,15 +1,16 @@
 <template>
-    <v-row class="pl-3" justify="center">
-        <v-sheet>
-            <v-card justify="center" class="mx-auto px-4 py-2" width="570px" rounded="0" elevation="0" link :to="{name: 'user', params: {userId: user.id}}">
+    <v-row  class="pl-3" justify="center" >
+        <v-sheet style="width: 570px;">
+            <v-card  class="mx-auto px-4 py-2" max-width="570px" rounded="0" elevation="0" link :to="{name: 'user', params: {userId: user.id}}">
+
                 <v-card-title class="d-flex pa-0">
                     <!-- ?A?C?R?? -->
                     <v-avatar :image="'../../storage/user/' + user.id + '/' + user.ICON" size="47"></v-avatar>
                     <!-- ?A?J?E???g?? -->
-                    <v-card-text style="font-size: 20px; " class="px-2 py-0 ">
-                        <v-card-title class="pa-0" style="height: 28px;">{{ user.USER_NAME }}</v-card-title>
+                    <v-card-text style="font-size: 20px; " class="px-1 py-0 ">
+                        <v-card-title class="pa-0" style="height: 28px; ">{{ user.USER_NAME }}</v-card-title>
 
-                        <v-card-subtitle class="pl-0"><v-icon class="mx-0">$at</v-icon>{{ user.id }}</v-card-subtitle>
+                        <v-card-subtitle class="pl-0" ><v-icon class="mx-0">$at</v-icon>{{ user.id }}</v-card-subtitle>
                     </v-card-text>
                     <!-- ???[?U?[ID -->
                     <!-- ??????? -->
@@ -17,8 +18,8 @@
                 </v-card-title>
             </v-card>
             <v-divider length="570px"></v-divider>
-        </v-sheet>
-    </v-row>
+        </v-sheet>   
+         </v-row>
 </template>
 
 <script>
